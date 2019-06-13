a7da79404c2658bc18cc1e6addf03b0502e6970b
congliux <congx.liu@intel.com>
2019-06-13 15:27:35 +0800

    update openvino to 2019_R1

--- products/tb3/modules/scripts/build_deps.sh
+++ products/tb3/modules/scripts/build_deps.sh
@@ -37,43 +37,59 @@ build_openvino_binary()
   local src_path=$1
   mkdir -p "${src_path}"/Openvino/openvino_binart
   cd "${src_path}"/Openvino/openvino_binart
-  wget -c http://registrationcenter-download.intel.com/akdlm/irc_nas/15078/l_openvino_toolkit_p_2018.5.455.tgz
-  tar -xvf l_openvino_toolkit_p_2018.5.455.tgz
-  cd l_openvino_toolkit_p_2018.5.455
-  sudo  ./install_cv_sdk_dependencies.sh
+  wget -c http://registrationcenter-download.intel.com/akdlm/irc_nas/15512/l_openvino_toolkit_p_2019.1.144.tgz
+  tar -xvf l_openvino_toolkit_p_2019.1.144.tgz
+  cd l_openvino_toolkit_p_2019.1.144
+  sudo ./install_openvino_dependencies.sh
+
   sed -i 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
-  if [ ! -d /opt/intel/computer_vision_sdk ];then
+  if [ ! -d /opt/intel/openvino ];then
     sudo ./install.sh --silent silent.cfg
   else
     echo "WARNING: Destination directory already exists."
   fi
-  cd /opt/intel/computer_vision_sdk/install_dependencies
-  sudo ./install_NEO_OCL_driver.sh
+  # cd /opt/intel/openvino/install_dependencies
+  # sudo ./install_NEO_OCL_driver.sh
 
   # shellcheck disable=SC1091
-  . /opt/intel/computer_vision_sdk/bin/setupvars.sh
+  . /opt/intel/openvino/bin/setupvars.sh
+  sudo rm -rf "${src_path}"/Openvino/openvino_binart_example
   mkdir -p "${src_path}"/Openvino/openvino_binart_example
   cd "${src_path}"/Openvino/openvino_binart_example
   mkdir -p build && cd build
-  cmake /opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/ && make && cd ..
-  sudo /bin/cp -rf build /opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/
+  cmake /opt/intel/openvino/deployment_tools/inference_engine/samples/ && make && cd ..
+  sudo /bin/cp -rf build /opt/intel/openvino/deployment_tools/inference_engine/samples/
 
-  cd /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/install_prerequisites
+  cd /opt/intel/openvino/deployment_tools/model_optimizer/install_prerequisites
   sudo ./install_prerequisites.sh
   mkdir -p ~/Downloads/models
   cd ~/Downloads/models
   wget http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
   tar -zxvf mask_rcnn_inception_v2_coco_2018_01_28.tar.gz
   cd mask_rcnn_inception_v2_coco_2018_01_28
-  python3 /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/mo_tf.py --input_model frozen_inference_graph.pb --tensorflow_use_custom_operations_config /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/extensions/front/tf/mask_rcnn_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --output_dir ./output/
+   python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py --input_model frozen_inference_graph.pb --tensorflow_use_custom_operations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/mask_rcnn_support.json --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --output_dir ./output/
   sudo mkdir -p /opt/models
   sudo ln -sf ~/Downloads/models/mask_rcnn_inception_v2_coco_2018_01_28 /opt/models/
-  cd /opt/intel/computer_vision_sdk/deployment_tools/model_downloader
+
+  cd /opt/intel/openvino/deployment_tools/tools/model_downloader
   sudo python3 ./downloader.py --name mobilenet-ssd
   #FP32 precision model
-  sudo python3 /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
+  sudo python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
   #FP16 precision model
-  sudo python3 /opt/intel/computer_vision_sdk/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16 --data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
+  sudo python3 /opt/intel/openvino/deployment_tools/model_optimizer/mo.py --input_model /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/mobilenet-ssd.caffemodel --output_dir /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16 --data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [127.5]
+  # download the optimized Intermediate Representation (IR) of model (excute once)
+  sudo python3 downloader.py --name face-detection-adas-0001
+  sudo python3 downloader.py --name face-detection-adas-0001-fp16
+  sudo python3 downloader.py --name age-gender-recognition-retail-0013
+  sudo python3 downloader.py --name emotions-recognition-retail-0003
+  sudo python3 downloader.py --name head-pose-estimation-adas-0001
+  sudo python3 downloader.py --name person-detection-retail-0013
+  sudo python3 downloader.py --name person-reidentification-retail-0076
+  sudo python3 downloader.py --name landmarks-regression-retail-0009
+  sudo python3 downloader.py --name face-reidentification-retail-0095
+  sudo python3 downloader.py --name vehicle-license-plate-detection-barrier-0106
+  sudo python3 downloader.py --name vehicle-attributes-recognition-barrier-0039
+  sudo python3 downloader.py --name license-plate-recognition-barrier-0001
   echo "Install OPENVINO SUCCESS!"
 
 }
@@ -139,7 +155,7 @@ main()
     echo "No such directory, create \"${deps_dir}\""
   fi
 
-  build_opencl "${deps_dir}"
+  # build_opencl "${deps_dir}"
   build_openvino_binary "${deps_dir}"
   build_opencv "${deps_dir}"
   build_librealsense "${deps_dir}"
--- products/tb3/modules/scripts/pre_build.sh
+++ products/tb3/modules/scripts/pre_build.sh
@@ -11,20 +11,21 @@ echo "$sdk_ws"
 sudo mkdir -p /opt/openvino_toolkit
 sudo ln -sf "$sdk_ws"/modules_ws/src/intel/ros2_openvino_toolkit /opt/openvino_toolkit
 
-sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/emotions-recognition/FP32/emotions-recognition-retail-0003.labels /opt/intel/computer_vision_sdk/deployment_tools/intel_models/emotions-recognition-retail-0003/FP32
-sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/face_detection/face-detection-adas-0001.labels /opt/intel/computer_vision_sdk/deployment_tools/intel_models/face-detection-adas-0001/FP32
-sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/face_detection/face-detection-adas-0001.labels /opt/intel/computer_vision_sdk/deployment_tools/intel_models/face-detection-adas-0001/FP16
+sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/emotions-recognition/FP32/emotions-recognition-retail-0003.labels /opt/intel/openvino/deployment_tools/intel_models/emotions-recognition-retail-0003/FP32
+sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/face_detection/face-detection-adas-0001.labels /opt/intel/openvino/deployment_tools/intel_models/face-detection-adas-0001/FP32
+sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/face_detection/face-detection-adas-0001.labels /opt/intel/openvino/deployment_tools/intel_models/face-detection-adas-0001/FP16
 sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_segmentation/frozen_inference_graph.labels ~/Downloads/models/mask_rcnn_inception_v2_coco_2018_01_28/output
-sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_detection/mobilenet-ssd.labels /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32
-sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_detection/mobilenet-ssd.labels /opt/intel/computer_vision_sdk/deployment_tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16
+sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_detection/mobilenet-ssd.labels /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP32
+ sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_detection/mobilenet-ssd.labels /opt/intel/openvino/deployment_tools/tools/model_downloader/object_detection/common/mobilenet-ssd/caffe/output/FP16
+sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/object_detection/vehicle-license-plate-detection-barrier-0106.labels /opt/intel/openvino/deployment_tools/tools/model_downloader/Security/object_detection/barrier/0106/dldt
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/build/intel64/Release/lib
-export CPU_EXTENSION_LIB=/opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libcpu_extension.so
-export GFLAGS_LIB=/opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libgflags_nothreads.a
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/openvino/deployment_tools/inference_engine/samples/build/intel64/Release/lib
+export CPU_EXTENSION_LIB=/opt/intel/openvino/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libcpu_extension.so
+export GFLAGS_LIB=/opt/intel/openvino/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libgflags_nothreads.a
 
 echo "export openvino env"
 # shellcheck disable=SC1091
-. /opt/intel/computer_vision_sdk/bin/setupvars.sh
+. /opt/intel/openvino/bin/setupvars.sh
 # Reset OpenCR to old version. Ref to [https://github.com/intel/ros2_openvino_toolkit/blob/devel/doc/BINARY_VERSION_README.md]
 export OpenCV_DIR=${sdk_ws}/third_party/opencv/opencv/build/
 
