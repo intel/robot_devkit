04c687019d857f9bec69b6ff2c963e0497358fcb
Chris Ye <chris.ye@intel.com>
2019-06-13 14:12:45 +0800

    update prebuild for perception build

--- packages/perception/scripts/pre_build.sh
+++ packages/perception/scripts/pre_build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Prepare for ROS2 build"
+echo "Prepare for [perception] build"
 
 set -e
 
-sdk_ws=$1
-echo "$sdk_ws"
+obj_dir=$1
+echo "Prebuild for [$obj_dir]"
+
+
+# openvino deps
 
 # Copy label files (excute once) OpenVINO
 sudo mkdir -p /opt/openvino_toolkit
-sudo ln -sf "$sdk_ws"/modules_ws/src/intel/ros2_openvino_toolkit /opt/openvino_toolkit
+sudo ln -sf "${obj_dir}"/src/intel/ros2_openvino_toolkit /opt/openvino_toolkit
 
 sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/emotions-recognition/FP32/emotions-recognition-retail-0003.labels /opt/intel/computer_vision_sdk/deployment_tools/intel_models/emotions-recognition-retail-0003/FP32
 sudo cp /opt/openvino_toolkit/ros2_openvino_toolkit/data/labels/face_detection/face-detection-adas-0001.labels /opt/intel/computer_vision_sdk/deployment_tools/intel_models/face-detection-adas-0001/FP32
@@ -22,29 +25,12 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/computer_vision_sdk/deploymen
 export CPU_EXTENSION_LIB=/opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libcpu_extension.so
 export GFLAGS_LIB=/opt/intel/computer_vision_sdk/deployment_tools/inference_engine/samples/build/intel64/Release/lib/libgflags_nothreads.a
 
-echo "export openvino env"
+# export openvino env
 # shellcheck disable=SC1091
 . /opt/intel/computer_vision_sdk/bin/setupvars.sh
 # Reset OpenCR to old version. Ref to [https://github.com/intel/ros2_openvino_toolkit/blob/devel/doc/BINARY_VERSION_README.md]
-export OpenCV_DIR=${sdk_ws}/third_party/opencv/opencv/build/
+export OpenCV_DIR=${obj_dir}/../third_party/opencv/build/
 
 sudo apt-get install -y \
   liblz4-1 \
   liblz4-dev
-
-# Configure the Neural Compute Stick USB Driver
-cd ~/Downloads
-cat <<EOF > 97-usbboot.rules
-SUBSYSTEM=="usb", ATTRS{idProduct}=="2150", ATTRS{idVendor}=="03e7", GROUP="users", MODE="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
-SUBSYSTEM=="usb", ATTRS{idProduct}=="2485", ATTRS{idVendor}=="03e7", GROUP="users", MODE="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
-SUBSYSTEM=="usb", ATTRS{idProduct}=="f63b", ATTRS{idVendor}=="03e7", GROUP="users", MODE="0666", ENV{ID_MM_DEVICE_IGNORE}="1"
-EOF
-sudo cp 97-usbboot.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-sudo ldconfig
-rm 97-usbboot.rules
-
-# Delete sudo time invalid operation
-echo "Delete sudo time invalid operation"
-sudo rm -rf /etc/sudoers.d/timeout
